eab427ba00cfd870efd304db6bf94bfe6815bc4f
Peter Molnar <molnarp@sztaki.mta.hu>
2013-08-26 14:47:13 +0200

    Added input reader classes.

--- src/main/java/de/komoot/hackathon/areaassigner/NodeInput.java
+++ src/main/java/de/komoot/hackathon/areaassigner/NodeInput.java
@@ -15,151 +15,56 @@
 
 package de.komoot.hackathon.areaassigner;
 
-import java.util.Iterator;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.Point;
 
-import eu.stratosphere.pact.common.contract.FileDataSink;
-import eu.stratosphere.pact.common.contract.FileDataSource;
-import eu.stratosphere.pact.common.contract.MapContract;
-import eu.stratosphere.pact.common.contract.ReduceContract;
-import eu.stratosphere.pact.common.contract.ReduceContract.Combinable;
-import eu.stratosphere.pact.common.io.RecordOutputFormat;
-import eu.stratosphere.pact.common.io.TextInputFormat;
-import eu.stratosphere.pact.common.plan.Plan;
-import eu.stratosphere.pact.common.plan.PlanAssembler;
-import eu.stratosphere.pact.common.plan.PlanAssemblerDescription;
+import de.komoot.hackathon.openstreetmap.GeometryModule;
+import de.komoot.hackathon.openstreetmap.JsonGeometryEntity;
 import eu.stratosphere.pact.common.stubs.Collector;
 import eu.stratosphere.pact.common.stubs.MapStub;
-import eu.stratosphere.pact.common.stubs.ReduceStub;
-import eu.stratosphere.pact.common.stubs.StubAnnotation.ConstantFields;
-import eu.stratosphere.pact.common.stubs.StubAnnotation.OutCardBounds;
 import eu.stratosphere.pact.common.type.PactRecord;
 import eu.stratosphere.pact.common.type.base.PactInteger;
 import eu.stratosphere.pact.common.type.base.PactString;
-import eu.stratosphere.pact.example.util.AsciiUtils;
 
-/**
- * Implements a word count which takes the input file and counts the number of
- * the occurrences of each word in the file.
- * 
- * @author Larysa, Moritz Kaufmann, Stephan Ewen
- */
-public class NodeInput implements PlanAssembler, PlanAssemblerDescription {
-
-	/**
-	 * Converts a PactRecord containing one string in to multiple string/integer
-	 * pairs. The string is tokenized by whitespaces. For each token a new
-	 * record is emitted, where the token is the first field and an Integer(1)
-	 * is the second field.
-	 */
-	@ConstantFields(fields = {})
-	@OutCardBounds(lowerBound = 0, upperBound = OutCardBounds.UNBOUNDED)
-	public static class TokenizeLine extends MapStub {
-		// initialize reusable mutable objects
-		private final PactRecord outputRecord = new PactRecord();
-		private final PactString word = new PactString();
-		private final PactInteger one = new PactInteger(1);
-
-		private final AsciiUtils.WhitespaceTokenizer tokenizer = new AsciiUtils.WhitespaceTokenizer();
-
-		@Override
-		public void map(PactRecord record, Collector<PactRecord> collector) {
-			// get the first field (as type PactString) from the record
-			PactString line = record.getField(0, PactString.class);
-
-			// normalize the line
-			AsciiUtils.replaceNonWordChars(line, ' ');
-			AsciiUtils.toLowerCase(line);
-
-			// tokenize the line
-			this.tokenizer.setStringToTokenize(line);
-			while (tokenizer.next(this.word)) {
-				// we emit a (word, 1) pair
-				this.outputRecord.setField(0, this.word);
-				this.outputRecord.setField(1, this.one);
-				collector.collect(this.outputRecord);
-			}
-		}
+public class NodeInput extends MapStub {
+	// initialize reusable mutable objects
+	private final PactRecord outputRecord = new PactRecord();
+	private final PactString nodeId = new PactString();
+	private final PactPoint point = new PactInteger(1);
+	private final ObjectMapper mapper;
+	
+	public NodeInput() {
+		mapper = new ObjectMapper();
+		mapper.registerModule(new GeometryModule());
+		mapper.configure(JsonGenerator.Feature.AUTO_CLOSE_TARGET, false);
 	}
-
-	/**
-	 * Sums up the counts for a certain given key. The counts are assumed to be
-	 * at position <code>1</code> in the record. The other fields are not
-	 * modified.
-	 */
-	@ConstantFields(fields = { 0 })
-	@OutCardBounds(lowerBound = 1, upperBound = 1)
-	@Combinable
-	public static class CountWords extends ReduceStub {
-		private final PactInteger cnt = new PactInteger();
-
-		@Override
-		public void reduce(Iterator<PactRecord> records,
-				Collector<PactRecord> out) throws Exception {
-			PactRecord element = null;
-			int sum = 0;
-			while (records.hasNext()) {
-				element = records.next();
-				PactInteger i = element.getField(1, PactInteger.class);
-				sum += i.getValue();
-			}
-
-			this.cnt.setValue(sum);
-			element.setField(1, this.cnt);
-			out.collect(element);
+	
+	@Override
+	public void map(PactRecord record, Collector<PactRecord> collector)
+			throws Exception {
+		
+		PactString line = record.getField(0, PactString.class);
+		
+		JsonGeometryEntity<Geometry> entry ;
+		
+		try {
+			entry = mapper.readValue(line.getValue(), JsonGeometryEntity.class);
+			
+			nodeId.setValue(entry.getId());
+			point.setValue((Point) entry.getGeometry());
+			
+			record.setField(0, nodeId);
+			record.setField(1, point);
+			
+			collector.collect(outputRecord);
+			
+		} catch (RuntimeException e) {
+			throw new RuntimeException("Unable to parse line: " + line + ":", e);
 		}
-
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see
-		 * eu.stratosphere.pact.common.stubs.ReduceStub#combine(java.util.Iterator
-		 * , eu.stratosphere.pact.common.stubs.Collector)
-		 */
-		@Override
-		public void combine(Iterator<PactRecord> records,
-				Collector<PactRecord> out) throws Exception {
-			// the logic is the same as in the reduce function, so simply call
-			// the reduce method
-			this.reduce(records, out);
-		}
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public Plan getPlan(String... args) {
-		// parse job parameters
-		int noSubTasks = (args.length > 0 ? Integer.parseInt(args[0]) : 1);
-		String dataInput = (args.length > 1 ? args[1] : "");
-		String output = (args.length > 2 ? args[2] : "");
-
-		FileDataSource source = new FileDataSource(TextInputFormat.class,
-				dataInput, "Input Lines");
-		source.setParameter(TextInputFormat.CHARSET_NAME, "ASCII"); // comment
-																	// out this
-																	// line for
-																	// UTF-8
-																	// inputs
-		MapContract mapper = MapContract.builder(TokenizeLine.class)
-				.input(source).name("Tokenize Lines").build();
-		ReduceContract reducer = new ReduceContract.Builder(CountWords.class,
-				PactString.class, 0).input(mapper).name("Count Words").build();
-		FileDataSink out = new FileDataSink(RecordOutputFormat.class, output,
-				reducer, "Word Counts");
-		RecordOutputFormat.configureRecordFormat(out).recordDelimiter('\n')
-				.fieldDelimiter(' ').lenient(true).field(PactString.class, 0)
-				.field(PactInteger.class, 1);
-
-		Plan plan = new Plan(out, "WordCount Example");
-		plan.setDefaultParallelism(noSubTasks);
-		return plan;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public String getDescription() {
-		return "Parameters: [noSubStasks] [input] [output]";
+		
+		
 	}
 
 }
