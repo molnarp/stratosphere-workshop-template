ec45abe24077b3adde71fe03c78141d6b420418c
Christoph Lingg <christoph@lingg.eu>
2013-08-26 14:57:21 +0200

    adding pact geometry

---
+++ src/main/java/de/komoot/hackathon/areaassigner/model/PactGeometry.java
@@ -0,0 +1,47 @@
+package de.komoot.hackathon.areaassigner.model;
+
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.io.ParseException;
+import com.vividsolutions.jts.io.WKBReader;
+import com.vividsolutions.jts.io.WKBWriter;
+import eu.stratosphere.pact.common.type.Value;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+/**
+ * @author christoph
+ * @date 26.08.13
+ */
+public class PactGeometry implements Value {
+	private Geometry geometry;
+
+	public Geometry getGeometry() {
+		return geometry;
+	}
+
+	@Override
+	public void write(DataOutput dataOutput) throws IOException {
+		WKBWriter writer = new WKBWriter();
+
+		byte[] bytes = writer.write(this.geometry);
+		dataOutput.writeInt(bytes.length);
+		dataOutput.write(bytes);
+	}
+
+	@Override
+	public void read(DataInput dataInput) throws IOException {
+		WKBReader reader = new WKBReader();
+
+		int size = dataInput.readInt();
+		byte[] bytes = new byte[size];
+		dataInput.readFully(bytes);
+
+		try {
+			this.geometry = reader.read(bytes);
+		} catch(ParseException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
