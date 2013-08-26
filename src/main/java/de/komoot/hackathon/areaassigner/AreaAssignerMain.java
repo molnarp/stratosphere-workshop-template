f56cd653516b70f7b52da46c217ebdeb92c53c70
Balázs <hajdu@ilab.sztaki.hu>
2013-08-26 15:25:49 +0200

    Created AreaAssignerMain.java

--- src/main/java/de/komoot/hackathon/areaassigner/AreaAssignerMain.java
+++ src/main/java/de/komoot/hackathon/areaassigner/AreaAssignerMain.java
@@ -180,7 +180,7 @@ public class AreaAssignerMain implements PlanAssembler, PlanAssemblerDescription
 	 * {@inheritDoc}
 	 */
 	public String getDescription() {
-		return "Parameters: [noSubStasks] [input] [output]";
+		return "Parameters: [noSubStasks] [nodeinput] [areainput] [output]";
 	}
 
 }
