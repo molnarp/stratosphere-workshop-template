1923d730446a0a4bdc897cfc20203178ac706384
Viktor <viktor@viktor-netbook.(none)>
2013-08-26 14:26:46 +0200

    area and point bounding box almoust done

--- src/main/java/de/komoot/hackathon/areaassigner/AreaBBox.java
+++ src/main/java/de/komoot/hackathon/areaassigner/AreaBBox.java
@@ -1,54 +1,23 @@
-/***********************************************************************************************************************
- *
- * Copyright (C) 2010 by the Stratosphere project (http://stratosphere.eu)
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- *
- **********************************************************************************************************************/
-
 package de.komoot.hackathon.areaassigner;
 
-import java.util.Iterator;
-
+import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.Point;
 
-
-import eu.stratosphere.pact.common.contract.FileDataSink;
-import eu.stratosphere.pact.common.contract.FileDataSource;
-import eu.stratosphere.pact.common.contract.MapContract;
-import eu.stratosphere.pact.common.contract.ReduceContract;
-import eu.stratosphere.pact.common.contract.ReduceContract.Combinable;
-import eu.stratosphere.pact.common.io.RecordOutputFormat;
-import eu.stratosphere.pact.common.io.TextInputFormat;
-import eu.stratosphere.pact.common.plan.Plan;
-import eu.stratosphere.pact.common.plan.PlanAssembler;
-import eu.stratosphere.pact.common.plan.PlanAssemblerDescription;
 import eu.stratosphere.pact.common.stubs.Collector;
 import eu.stratosphere.pact.common.stubs.MapStub;
-import eu.stratosphere.pact.common.stubs.ReduceStub;
 import eu.stratosphere.pact.common.stubs.StubAnnotation.ConstantFields;
 import eu.stratosphere.pact.common.stubs.StubAnnotation.OutCardBounds;
 import eu.stratosphere.pact.common.type.PactRecord;
-import eu.stratosphere.pact.common.type.base.PactInteger;
-import eu.stratosphere.pact.common.type.base.PactString;
-import eu.stratosphere.pact.example.util.AsciiUtils;
 
 @ConstantFields(fields = {0, 1})
 @OutCardBounds(lowerBound = 1, upperBound = 1)
 public class AreaBBox extends MapStub {
 
   @Override
-  public void map(PactRecord pointRecord, Collector<PactRecord> out) throws Exception {
-    Point point = pointRecord.getField(1, Point.class);
-    pointRecord.addField(point.getBoundary().getEnvelopeInternal());
-    out.collect(pointRecord);
+  public void map(PactRecord areaRecord, Collector<PactRecord> out) throws Exception {
+    Geometry geometry = areaRecord.getField(1, Geometry.class);
+    areaRecord.addField(geometry.getEnvelopeInternal());
+    out.collect(areaRecord);
   }
   
 }
