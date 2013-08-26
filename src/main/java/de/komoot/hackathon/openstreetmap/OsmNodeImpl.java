9763ad03abc7f1ba6ac72d3d3e5bb5403e6529a0
jan <jan@komoot.de>
2013-08-26 18:38:09 +0200

    simplified the points

--- src/main/java/de/komoot/hackathon/openstreetmap/OsmNodeImpl.java
+++ src/main/java/de/komoot/hackathon/openstreetmap/OsmNodeImpl.java
@@ -1,25 +1,48 @@
 package de.komoot.hackathon.openstreetmap;
 
 import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.PrecisionModel;
 
 import java.util.Map;
 
 /** @author jan */
-public class OsmNodeImpl extends OsmEntityImpl<Point> implements OsmNode {
+public class OsmNodeImpl implements OsmNode {
 
-	public OsmNodeImpl(long osmId, Point geometry) {
-		super(osmId, geometry);
+	private final static GeometryFactory gf = new GeometryFactory(new PrecisionModel(), 4326);
+
+	private final long id;
+
+	private final Coordinate coordinate;
+
+	public OsmNodeImpl(long osmId, Coordinate coordinate) {
+		this.id = osmId;
+		this.coordinate = coordinate;
 	}
 
 	@Override
 	public Coordinate getCoordinate() {
-		Point g = getGeometry();
-		return g == null ? null : g.getCoordinate();
+		return coordinate;
+	}
+
+	@Override
+	public long getOsmId() {
+		return id;
 	}
 
 	@Override
 	public String getId() {
 		return "N" + getOsmId();
+	}
+
+	@Override
+	public Point getGeometry() {
+		return gf.createPoint(coordinate);
+	}
+
+	@Override
+	public Map<String, String> getTags() {
+		return null;
 	}
 }
