76183b9c858c8ffd9491657e434ef57eaea28d92
Viktor <viktor@viktor-netbook.(none)>
2013-08-27 16:33:41 +0200

    fix

--- src/main/java/de/komoot/hackathon/areaassigner/utils/NewGrid.java
+++ src/main/java/de/komoot/hackathon/areaassigner/utils/NewGrid.java
@@ -45,8 +45,12 @@ public class NewGrid {
 	}
 
 	private String getTileId(final int x, final int y) {
-		return ((double) x / (1 << zoom) * 360 - 180.0) + "," + ((double) (x + 1) / (1 << zoom) * 360 - 180.0) + "," +
-		    ((double) y / (1 << zoom) * 180 - 90.0) + "," + ((double) (y + 1) / (1 << zoom) * 180 - 90.0);
+	  double minX = tile2lon(x);
+    double maxX = tile2lon(x + 1);
+    double minY = tile2lat(y);
+    double maxY = tile2lat(y + 1);
+    
+		return minX + "," + maxX + "," + minY + "," + maxY;
 	}
 
 	private int getX(double lon) {
@@ -54,7 +58,15 @@ public class NewGrid {
 	}
 
   private int getY(double lat) {
-    return (int) Math.floor((lat + 90) / 180 * (1 << zoom));
+    return (int) Math.floor((1 - Math.log(Math.tan(Math.toRadians(lat)) + 1 / Math.cos(Math.toRadians(lat))) / Math.PI) / 2 * (1 << zoom));
   }
-	
+  
+  private double tile2lon(int x) {
+    return x / Math.pow(2.0, zoom) * 360.0 - 180;
+  }
+
+  private double tile2lat(int y) {
+    double n = Math.PI - (2.0 * Math.PI * y) / Math.pow(2.0, zoom);
+    return Math.toDegrees(Math.atan(Math.sinh(n)));
+  }
 }
